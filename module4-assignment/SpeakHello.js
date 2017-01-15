02921b80fa458ef425a733f77d325298fd1f0446
Johan R <johanro@gmail.com>
2017-01-15 14:59:14 +0100

    Updated a minor error

--- module4-assignment/SpeakHello.js
+++ module4-assignment/SpeakHello.js
@@ -12,18 +12,12 @@ var helloSpeaker = {
 	}
 };
 
-console.log(helloSpeaker);
-
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-
-//function helloSpeaker.speak(name) {
-//  
-// }
 
 window.helloSpeaker = helloSpeaker;
 
